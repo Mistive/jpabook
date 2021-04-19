e199716fe14b5196ec0857dfa5a390028e3c5c7e
mistive <pms9725@gmail.com>
2021-04-19 23:54:48 +0900

    실전 예제2

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,8 +19,17 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+            //1. 연관 편의 메소드를 활용
+            //Order order = new Order();
+            //order.addOrderItem(new OrderItem());
 
+            //2. 단방향일 경우 단순히 persist()를 통해 커맨드 작성
+            Order order = new Order();
+            em.persist(order);
 
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,19 +1,39 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
+@Getter @Setter
 public class Order {
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
     private Long id;
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    //양방향 편의 메소드
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);  //현재 내가 가지고 있는 ItemList에 orderItem을 추가하고,
+        orderItem.setOrder(this);   //주문아이템의 주문 내역에 나를 추가한다.
+    }
 }
